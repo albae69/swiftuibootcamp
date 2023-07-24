edcc0be77e600c467fb031e6ba24929849aa278a
albae69 <ahmadalbaihaqi69@gmail.com>
2023-07-24 11:12:45 +0700

    Initial Commit

---
+++ swiftuibootcamp/swiftuibootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuibootcampApp.swift
+//  swiftuibootcamp
+//
+//  Created by co2 on 24/07/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftuibootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
